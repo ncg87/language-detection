625dbcb97a7020ab681a632e99fc60a54f532991
ncg87 <115420183+ncg87@users.noreply.github.com>
2024-07-04 19:35:31 -0400

    done

--- dataset.py
+++ dataset.py
@@ -3,14 +3,17 @@ from torch.utils.data import Dataset
 import pandas as pd
 
 class LanguageDetectionDataset(Dataset):
+    """Small language detection dataset. Contains text and language columns for 17 different languages."""
     def __init__(self, path):
+        # Load data from csv file
         self.data = pd.read_csv(path)
+        # Get text and language columns
         self.text = self.data['Text'].tolist()
         self.language = self.data['Language'].tolist()
-
+    # Return the length of the dataset
     def __len__(self):
         return len(self.data)
-
+    # Return text and language at specific index
     def __getitem__(self, idx):
         text = self.text[idx]
         language = self.language[idx]
